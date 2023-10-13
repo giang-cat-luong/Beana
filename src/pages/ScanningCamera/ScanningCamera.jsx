a8bc5828904374280aa40c5d217cff16661f9048
truonggiang0405 <vutruonggiang452002@gmail.com>
2023-10-13 19:46:10 +0700

    test camera safari

--- src/pages/ScanningCamera/ScanningCamera.jsx
+++ src/pages/ScanningCamera/ScanningCamera.jsx
@@ -60,13 +60,13 @@ function ScanningCamera() {
     }
 
     return (
-        <div className="flex w-[100vw] h-[100vh] flex-col items-center justify-between">
+        <div className="flex flex-col items-center justify-between relative">
             <h1>FAce Detection</h1>
             <div className="px-14">
                 <video  crossOrigin="anonymous" ref={videoRef} autoPlay></video>
             </div>
             <canvas ref={canvasRef} width="640" height="550"
-                className="absolute top-[350px]" />
+                className="absolute top-[-60%]" />
         </div>
     )
 
