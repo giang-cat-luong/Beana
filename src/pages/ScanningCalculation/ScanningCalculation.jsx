bde410089a612d8bdf555a1273d42532a2f7d4d2
truonggiang0405 <vutruonggiang452002@gmail.com>
2023-11-15 18:49:42 +0700

    fix face scanning

--- src/pages/ScanningCalculation/ScanningCalculation.jsx
+++ src/pages/ScanningCalculation/ScanningCalculation.jsx
@@ -361,14 +361,94 @@ function ScanningCalculation() {
                 <div className='relative'>
                     <div className="h-[620px] z-0 bg-fixed" style={{ backgroundImage: `url(${img})` }}>
                         <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
-                            <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
-                            <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
-                            <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
-                            <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
-                            <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
-                            <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
-                            <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                            <img className='w-20 absolute top-60 left-[23%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700048340/Beana_assets_analyze/matrai_v1t691.png' />
+                            <img className='w-20 absolute top-60 right-[23%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700048340/Beana_assets_analyze/maphai_wrrr6p.png' />
                         </div>
+                        {/* {analyzeCountDown >= 7
+                            ? (
+                                <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                    <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
+                                    <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
+                                    <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
+                                    <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
+                                    <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
+                                    <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
+                                    <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                                </div>
+                            ) : analyzeCountDown >= 6 ?
+                                (
+                                    <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                        <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700048340/Beana_assets_analyze/matrai_v1t691.png' />
+                                        <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700048340/Beana_assets_analyze/maphai_wrrr6p.png' />
+                                    </div>
+                                ) : analyzeCountDown >= 5 ?
+                                    (
+                                        <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                            <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
+                                            <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
+                                            <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
+                                            <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
+                                            <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
+                                            <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
+                                            <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                                        </div>
+                                    ) : analyzeCountDown >= 4 ?
+                                        (
+                                            <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                                <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
+                                                <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
+                                                <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
+                                                <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
+                                                <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
+                                                <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
+                                                <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                                            </div>
+                                        ) : analyzeCountDown >= 3 ?
+                                            (
+                                                <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                                    <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
+                                                    <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
+                                                    <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
+                                                    <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
+                                                    <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
+                                                    <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
+                                                    <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                                                </div>
+                                            ) : analyzeCountDown >= 2 ?
+                                                (
+                                                    <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                                        <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
+                                                        <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
+                                                        <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
+                                                        <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
+                                                        <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
+                                                        <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
+                                                        <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                                                    </div>
+                                                ) : analyzeCountDown >= 1 ?
+                                                    (
+                                                        <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                                            <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
+                                                            <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
+                                                            <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
+                                                            <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
+                                                            <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
+                                                            <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
+                                                            <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                                                        </div>
+                                                    ) : analyzeCountDown >= 0 &&
+                                                    (
+                                                        <div className="h-[620px] bg-black/40 backdrop-opacity-10 backdrop-invert">
+                                                            <img className='w-36 absolute top-40 left-1/2 -translate-x-1/2 opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/tran_qxc9hz.png' />
+                                                            <img className='w-10 absolute top-[262px] left-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-ma_sspixz.png' />
+                                                            <img className='w-6 absolute bottom-[266px] left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045965/Beana_assets_analyze/left-camtren_fcod48.png' />
+                                                            <img className='w-10 absolute bottom-52 left-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/left-camduoi_ud7fxt.png' />
+                                                            <img className='w-10 absolute top-[262px] right-[28%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-ma_qdbws4.png' />
+                                                            <img className='w-6 absolute bottom-[266px] right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/right-camtren_es1drv.png' />
+                                                            <img className='w-10 absolute bottom-52 right-[35%] opacity-30' src='https://res.cloudinary.com/dc4hafqoa/image/upload/v1700045964/Beana_assets_analyze/rightcamduoi_mgz4jw.png' />
+                                                        </div>
+                                                    )
+                        } */}
                         <div className='w-full absolute bottom-20 left-[50%] text-center -translate-x-1/2 mb-6'>
                             <div className='flex justify-center text-white mb-4 text-xs'>
                                 {analyzeCountDown >= 6
